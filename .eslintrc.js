bd51578debbd646fbd744b6fd223daaa2e192c8a
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 15:21:11 +0800

    feat(lint) update config use standard

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    // React
+    'react/prop-types': 'off',
+
+    // Import
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+
+    // Other
+    'no-use-before-define': 'off',
+    'multiline-ternary': 'off'
+  }
+}
