e6cf12af1430f7b9a9459ea41c71b401a4c19941
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 17:13:12 +0800

    feat(home) init

---
+++ src/pages-main/Home/Home.main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import About from './About'
+import Hero from './Hero'
+// import Projects from './Projects'
+// import Stack from './Stack'
+
+const Home = (): JSX.Element => (
+  <>
+    <Hero />
+    {/* <About /> */}
+    {/*  <Stack />
+    <Projects /> */}
+  </>
+)
+
+export default Home
