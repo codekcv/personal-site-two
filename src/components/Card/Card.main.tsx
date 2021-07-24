ea15249737e24e2d66b3ebc8838af8dd700b3570
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 17:27:32 +0800

    ref(card) use styled

--- src/components/Card/Card.main.tsx
+++ src/components/Card/Card.main.tsx
@@ -1,20 +1,15 @@
-import { motion } from 'framer-motion'
-import React from 'react'
+import styled from '@emotion/styled'
 
 import { MotionBox } from '../Motion/Motion.main'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Card: React.FC<any> = (props) => (
-  <MotionBox
-    as={motion.div}
-    backgroundColor="white"
-    padding="2rem"
-    cursor="pointer"
-    border="1px dotted rgba(0, 0, 0, 0.1)"
-    borderRadius={12}
-    boxShadow="0 0.75rem 3rem 0em rgba(0, 0, 0, 0.05), 0 0.75rem 0rem 0rem rgba(0, 0, 0, 0.05)"
-    {...props}
-  />
-)
+const Card = styled(MotionBox)`
+  background-color: white;
+  padding: 2rem;
+  cursor: pointer;
+  border: 1px dotted rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  box-shadow: 0 0.75rem 3rem 0em rgba(0, 0, 0, 0.05),
+    0 0.75rem 0rem 0rem rgba(0, 0, 0, 0.05);
+`
 
 export default Card
