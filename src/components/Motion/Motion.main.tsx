6872359bc155ba7dd68cff24ebaa26c39c71292b
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 17:12:18 +0800

    feat: motion components

---
+++ src/components/Motion/Motion.main.tsx
@@ -0,0 +1,6 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+
+export const MotionBox = motion(Box)
+export const MotionFlex = motion(Flex)
+export const MotionText = motion(Text)
