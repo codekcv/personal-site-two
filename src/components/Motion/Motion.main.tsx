2fc7570ff966d310b8a225ca43d5e48293953a44
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 22:22:58 +0800

    feat: motion heading comp

--- src/components/Motion/Motion.main.tsx
+++ src/components/Motion/Motion.main.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 
 export const MotionBox = motion(Box)
 export const MotionFlex = motion(Flex)
 export const MotionText = motion(Text)
+export const MotionHeading = motion(Heading)
