4cfeb76015fd55f0f40820a97adfd0c77839da90
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 17:28:03 +0800

    ref(home): extract layout to main home comp

--- src/pages-main/Home/Home.main.tsx
+++ src/pages-main/Home/Home.main.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
+import Layout from '../../components/Layout'
 import About from './About'
 import Hero from './Hero'
-// import Projects from './Projects'
-// import Stack from './Stack'
+import Projects from './Projects'
+import Stack from './Stack'
 
 const Home = (): JSX.Element => (
-  <>
+  <Layout>
     <Hero />
-    {/* <About /> */}
-    {/*  <Stack />
-    <Projects /> */}
-  </>
+    <About />
+    <Stack />
+    <Projects />
+  </Layout>
 )
 
 export default Home
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,1 @@
-import * as React from 'react'
-
-import Layout from '../components/Layout'
-import Home from '../pages-main/Home'
-
-const IndexPage = (): JSX.Element => {
-  return (
-    <Layout>
-      <Home />
-    </Layout>
-  )
-}
-
-export default IndexPage
+export { default } from '../pages-main/Home'
