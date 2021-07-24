c6cad959f1aa6d08e8a48fb225035a18ee0e5ad3
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-24 15:22:12 +0800

    feat(pkg) eslint configs and update gatsby conf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "personal-site-two",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'personal-site-two'
   },
   plugins: [
-    {
-      resolve: "gatsby-plugin-typescript",
-      options: {
-        isTSX: true,
-        jsxPragma: "react-jsx",
-        allExtensions: true,
-      },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "@chakra-ui/gatsby-plugin",
-  ],
-};
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    '@chakra-ui/gatsby-plugin',
+    'gatsby-plugin-eslint'
+  ]
+}
