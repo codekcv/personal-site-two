29156acd2865ba5698b6e64ad1d4948e5118dfa6
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-25 13:20:01 +0800

    feat(stack) use bobble title and card

--- src/pages-main/Home/Stack/Stack.main.tsx
+++ src/pages-main/Home/Stack/Stack.main.tsx
@@ -1,14 +1,15 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 
-import Bobble from '../../../components/Bobble'
+import BobbleTitle from '../../../components/BobbleTitle'
 import Card from '../../../components/Card'
-import { MotionHeading } from '../../../components/Motion/Motion.main'
-import { animDelay, animDuration, triggerOnce } from '../Home.util'
+import { animDelay, triggerOnce } from '../Home.util'
 import { backEndTechnologies, frontEndTechnologies } from './Stack.util'
 
 const Stack = (): JSX.Element => {
+  const [isOpenBE, setIsOpenBE] = useState(false)
+  const [isOpenFE, setIsOpenFE] = useState(false)
   const { ref, inView } = useInView({
     threshold: 0.5,
     delay: animDelay,
@@ -16,73 +17,44 @@ const Stack = (): JSX.Element => {
   })
 
   return (
-    <Box mt="2rem">
-      <Box position="relative" zIndex={1}>
-        <Bobble>
-          <MotionHeading
-            as="h1"
-            fontSize="4rem"
-            textAlign="center"
-            textShadow="0 0.35rem 0px rgba(0,0,0,0.1)"
-            variants={{
-              out: {
-                opacity: 0,
-                transform: 'scale(1.5)  translateY(6rem)'
-              },
-              in: { opacity: 1, transform: 'scale(1)  translateY(0rem)' }
-            }}
-            initial="out"
-            animate={inView ? 'in' : 'out'}
-            transition={{ duration: animDuration, ease: 'easeOut' }}
-          >
-            Technology Stack
-          </MotionHeading>
-        </Bobble>
-      </Box>
+    <Box ref={ref} mt="2rem">
+      <BobbleTitle inView={inView} isOpen={isOpenBE || isOpenFE}>
+        Technology Stack
+      </BobbleTitle>
 
-      <Box position="relative" mt="-1.5rem" zIndex={0} ref={ref}>
-        <Flex justifyContent="center">
-          <Card
-            width="50%"
-            mr="2rem"
-            variants={{
-              out: { opacity: 0, transform: 'translateX(-6rem)' },
-              in: { opacity: 1, transform: 'translateX(0rem)' }
-            }}
-            initial="out"
-            animate={inView ? 'in' : 'out'}
-            transition={{ duration: animDuration, ease: 'easeOut' }}
-          >
-            <Text as="h2" textAlign="center">
-              Back-End
-            </Text>
+      <Flex justifyContent="center">
+        <Card
+          isOpen={isOpenBE}
+          setIsOpen={setIsOpenBE}
+          w="50%"
+          mr="2rem"
+          alignSelf="stretch"
+        >
+          <Text as="h2" textAlign="center">
+            Back-End
+          </Text>
 
-            <Flex mt="1.5rem" flexWrap="wrap" justifyContent="center">
-              {backEndTechnologies}
-            </Flex>
-          </Card>
+          <Flex mt="1.5rem" flexWrap="wrap" justifyContent="center">
+            {backEndTechnologies}
+          </Flex>
+        </Card>
 
-          <Card
-            width="50%"
-            ml="2rem"
-            variants={{
-              out: { opacity: 0, transform: 'translateX(6rem)' },
-              in: { opacity: 1, transform: 'translateX(0rem)' }
-            }}
-            initial="out"
-            animate={inView ? 'in' : 'out'}
-            transition={{ duration: animDuration, ease: 'easeOut' }}
-          >
-            <Text as="h2" textAlign="center">
-              Front-End
-            </Text>
+        <Card
+          isOpen={isOpenFE}
+          setIsOpen={setIsOpenFE}
+          w="50%"
+          ml="2rem"
+          alignSelf="stretch"
+        >
+          <Text as="h2" textAlign="center">
+            Front-End
+          </Text>
 
-            <Flex mt="1.5rem" flexWrap="wrap" justifyContent="center">
-              {frontEndTechnologies}
-            </Flex>
-          </Card>
-        </Flex>
-      </Box>
+          <Flex mt="1.5rem" flexWrap="wrap" justifyContent="center">
+            {frontEndTechnologies}
+          </Flex>
+        </Card>
+      </Flex>
     </Box>
   )
 }
