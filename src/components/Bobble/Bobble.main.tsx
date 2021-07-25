0a2cbc051603dd0aeaabdb2b76adff010ace2bcb
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-25 09:28:28 +0800

    ref(bobble) allow pass box properties

--- src/components/Bobble/Bobble.main.tsx
+++ src/components/Bobble/Bobble.main.tsx
@@ -1,35 +1,41 @@
+import { BoxProps } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import React from 'react'
 
+import { MotionFlex } from '../Motion/Motion.main'
 import { bobbingRange, maxVary, minVary } from './Bobble.util'
 
-const Bobble: React.FC = ({ children }) => (
-  <motion.div
-    animate={{ transform: bobbingRange.x.transform }}
-    transition={{
-      repeat: Infinity,
-      duration:
-        bobbingRange.x.duration /
-        (Math.random() * (maxVary - minVary) + minVary)
-    }}
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}
-  >
-    <motion.div
-      animate={{ transform: bobbingRange.y.transform }}
+const Bobble: React.FC<BoxProps> = (props) => {
+  const { children } = props
+  const passedProps = { ...props }
+  delete passedProps.children
+
+  return (
+    <MotionFlex
+      justifyContent="center"
+      alignItems="center"
+      animate={{ transform: bobbingRange.x.transform }}
       transition={{
         repeat: Infinity,
         duration:
-          bobbingRange.y.duration /
+          bobbingRange.x.duration /
           (Math.random() * (maxVary - minVary) + minVary)
       }}
+      {...passedProps}
     >
-      {children}
-    </motion.div>
-  </motion.div>
-)
+      <motion.div
+        animate={{ transform: bobbingRange.y.transform }}
+        transition={{
+          repeat: Infinity,
+          duration:
+            bobbingRange.y.duration /
+            (Math.random() * (maxVary - minVary) + minVary)
+        }}
+      >
+        {children}
+      </motion.div>
+    </MotionFlex>
+  )
+}
 
 export default Bobble
