ef2a62de8080ca6d2f51b571190d0a491fbb603a
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-26 18:23:16 +0800

    log mount

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,32 @@
 import { Box, Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Footer from './Footer'
 import Header from './Header'
 
-const Layout: React.FC = ({ children }) => (
-  <Flex
-    as="main"
-    flexDirection="column"
-    justifyContent="center"
-    alignItems="center"
-    minHeight="100vh"
-    maxWidth={768}
-    mx="auto"
-  >
-    <Box w="100%" flex={1}>
-      <Header />
-      {children}
-    </Box>
+const Layout: React.FC = ({ children }) => {
+  useEffect(() => {
+    console.log('Mounted')
+  }, [])
 
-    <Footer />
-  </Flex>
-)
+  return (
+    <Flex
+      as="main"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      maxWidth={768}
+      mx="auto"
+    >
+      <Box w="100%" flex={1}>
+        <Header />
+        {children}
+      </Box>
+
+      <Footer />
+    </Flex>
+  )
+}
 
 export default Layout
