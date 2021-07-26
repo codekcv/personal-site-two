d135e1f36e60e110493bf66feba962bc2c601a37
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-26 15:39:02 +0800

    feat(about) info update

--- src/pages-main/index/About.tsx
+++ src/pages-main/index/About.tsx
@@ -8,7 +8,6 @@ import Card from '../../components/Card'
 import { animDelay, triggerOnce } from '../../utils/constants'
 
 const facts = [
-  'My name is Christian Villamin; I live in Manila, Philippines.',
   <>
     I&apos;m currently working at&nbsp;
     <a href="https://localhost/" target="_blank" rel="noopener noreferrer">
@@ -64,16 +63,145 @@ const About = (): JSX.Element => {
         }
         contentFull={
           <>
-            <Heading as="h1">Hi, I&apos;m Christian Villamin!</Heading>
+            <Heading as="h1">Hello, I&apos;m Christian Villamin!</Heading>
 
             <Text mt="1rem">
-              i&apos;m a software engineer specializing on the web. i love
+              {`{...}`}
+              {/* i&apos;m a software engineer specializing on the web. i love
               refactoring and optimizing stuff. i also animate things. coding
               god. tinkerer. loves winning. i don&apos;t play games but i play
               sometimes. memes. piano. cats. coffee. immortality. no interest in
               politics and religion, i respect your view. nature is beautiful
-              and it is life. detective conan fanboi. happy kid :&#41;
+              and it is life. detective conan fanboi. happy kid :&#41; */}
             </Text>
+
+            <Box mt="2rem">
+              <Heading as="h2">Programming History</Heading>
+
+              <Text mt="1rem">
+                <a
+                  href="https://www.thehelper.net/members/nherwyziant.2291/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  I started programming in 2007 when I was 11
+                </a>
+                , using Blizzard&apos;s event-driven scripting language
+                called&nbsp;
+                <a
+                  href="https://www.hiveworkshop.com/threads/beginning-jass-tutorial-series.30765/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  JASS
+                </a>
+                &nbsp;to make modifications and create custom maps in Warcraft
+                III. I learned it by reading guides &amp; documentations online,
+                and since then have enjoyed the art of programming to heart. I
+                made tower defense games, battle arenas, hero campaign maps,
+                etc. with it and played it with my local and online friends.
+              </Text>
+
+              <Text mt="1rem">
+                After that, I learned to program Java in 2012 to build basic
+                desktop app. In 2013, I joined a programming competition that
+                deals with solving algorithms and&nbsp;
+                <a
+                  href="https://codekcv.github.io/competition.jpg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  I won
+                </a>
+                . In 2014, I decided to make a mobile game using the Android
+                Studio IDE and an awesome library called&nbsp;
+                <a
+                  href="https://libgdx.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  libGDX
+                </a>
+                . I made a 2D platform pixel-art running game. In 2016, I
+                decided to learn C# &amp; the basics of Blender, and moved to
+                Unity3D to make 1st person games, although nothing serious, just
+                for fun and messing around with my friends. I did learn maths in
+                3D, so that&apos;s nice.
+              </Text>
+
+              <Text mt="1rem">
+                And now starting year, 2019, I&apos;m diving deep in the
+                beautiful world of web development and beyond.
+              </Text>
+            </Box>
+
+            <Box mt="2rem">
+              <Heading as="h2">Programming History</Heading>
+
+              <Text mt="1rem">
+                <a
+                  href="https://www.thehelper.net/members/nherwyziant.2291/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  I started programming in 2007 when I was 11
+                </a>
+                , using Blizzard&apos;s event-driven scripting language
+                called&nbsp;
+                <a
+                  href="https://www.hiveworkshop.com/threads/beginning-jass-tutorial-series.30765/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  JASS
+                </a>
+                &nbsp;to make modifications and create custom maps in Warcraft
+                III. I learned it by reading guides &amp; documentations online,
+                and since then have enjoyed the art of programming to heart. I
+                made tower defense games, battle arenas, hero campaign maps,
+                etc. with it and played it with my local and online friends.
+              </Text>
+
+              <Text mt="1rem">
+                After that, I learned to program Java in 2012 to build basic
+                desktop app. In 2013, I joined a programming competition that
+                deals with solving algorithms and&nbsp;
+                <a
+                  href="https://codekcv.github.io/competition.jpg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  I won
+                </a>
+                . In 2014, I decided to make a mobile game using the Android
+                Studio IDE and an awesome library called&nbsp;
+                <a
+                  href="https://libgdx.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  libGDX
+                </a>
+                . I made a 2D platform pixel-art running game. In 2016, I
+                decided to learn C# &amp; the basics of Blender, and moved to
+                Unity3D to make 1st person games, although nothing serious, just
+                for fun and messing around with my friends. I did learn maths in
+                3D, so that&apos;s nice.
+              </Text>
+
+              <Text mt="1rem">
+                And now starting year, 2019, I&apos;m diving deep in the
+                beautiful world of web development and beyond.
+              </Text>
+            </Box>
+
+            <Box mt="2rem">
+              <Heading as="h2">Interests</Heading>
+
+              <Text mt="1rem">
+                Animals, Detective Conan, Doctor Who, Kingdom Hearts
+              </Text>
+            </Box>
           </>
         }
       />
