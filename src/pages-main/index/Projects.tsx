e80b61a231969d7d7da73979c797b3cbd0246b82
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-28 13:09:32 +0800

    feat(projects) init fcc

--- src/pages-main/index/Projects.tsx
+++ src/pages-main/index/Projects.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 import { StaticImage } from 'gatsby-plugin-image'
 import React, { useState } from 'react'
 import { useInView } from 'react-intersection-observer'
@@ -21,11 +21,25 @@ const projects = [
         alt="An open-source pluggable full-stack headless commerce solution made with TypeScript, React, Node.js, and GraphQL."
       />
     )
+  },
+  {
+    title: 'freeCodeCamp Projects',
+    description: 'All my 30 freeCodeCamp projects.',
+    img: (
+      <StaticImage
+        src="../../images/freecodecamp.png"
+        width={700}
+        height={340}
+        placeholder="blurred"
+        alt="An open-source pluggable full-stack headless commerce solution made with TypeScript, React, Node.js, and GraphQL."
+      />
+    )
   }
 ].map((item) => <Box key={item.title}>{item.img}</Box>)
 
 const Projects = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false)
+  const [x, y] = useState(false)
 
   const { ref, inView } = useInView({
     threshold: 0.7,
@@ -33,19 +47,57 @@ const Projects = (): JSX.Element => {
     triggerOnce
   })
 
+  const { ref: refB, inView: inViewB } = useInView({
+    threshold: 0.7,
+    delay: animDelay,
+    triggerOnce
+  })
+
   return (
-    <Box ref={ref} mt="2rem" onClick={() => setIsOpen(!isOpen)}>
+    <Box mt="2rem" onClick={() => setIsOpen(!isOpen)}>
       <BobbleTitle inView={inView} isOpen={isOpen}>
         Project
       </BobbleTitle>
 
       <Card
+        inViewRef={ref}
         inView={inView}
         isOpen={isOpen}
         setIsOpen={setIsOpen}
         mt="-1.5rem"
-        contentPrev={projects}
-        contentFull={null}
+        contentPrev={<Flex justifyContent="center">{projects[0]}</Flex>}
+        contentFull={
+          <Box>
+            <Heading as="h1">Headless Commerce Project</Heading>
+
+            <Text mt="0.5rem">
+              Source:&nbsp;
+              <a
+                href="https://github.com/codekcv/headless-commerce"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://github.com/codekcv/headless-commerce
+              </a>
+            </Text>
+          </Box>
+        }
+      />
+
+      <Card
+        inViewRef={refB}
+        inView={inViewB}
+        isOpen={x}
+        setIsOpen={y}
+        mt="2rem"
+        contentPrev={<Flex justifyContent="center">{projects[1]}</Flex>}
+        contentFull={
+          <Box>
+            <Heading as="h1">freeCodeCamp Projects</Heading>
+
+            <Text mt="0.5rem">These are my 30 freeCodeCamp projects.</Text>
+          </Box>
+        }
       />
     </Box>
   )
